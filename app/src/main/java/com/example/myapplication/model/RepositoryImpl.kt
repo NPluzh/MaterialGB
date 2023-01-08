4d8482fa5d08b1041b6a2d6771e124995887874a
whittema <shamarsen@mail.ru>
2023-01-08 15:36:17 +0300

    урок 1| добавил модель

---
+++ app/src/main/java/com/example/myapplication/model/RepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication.model
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class RepositoryImpl:Repository {
+    private val baseUrl = "https://api.nasa.gov/"
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().setLenient().create()))
+            .build()
+    }
+
+    private fun getPictureOfTheDayApi():PictureOfTheDayAPI{
+     return retrofit.create(PictureOfTheDayAPI::class.java)
+    }
+}
