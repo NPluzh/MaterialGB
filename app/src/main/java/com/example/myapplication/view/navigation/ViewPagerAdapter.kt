46d35289878025096dcf0a44ae528679a83ba3db
whittema <shamarsen@mail.ru>
2023-02-03 19:08:35 +0300

    урок 3| добавил индикатор промотки

--- app/src/main/java/com/example/myapplication/view/navigation/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/myapplication/view/navigation/ViewPagerAdapter.kt
@@ -1,11 +1,8 @@
-package com.gb.m_2090_3.view.navigation
+package com.example.myapplication.view.navigation
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
-import com.example.myapplication.view.navigation.EarthFragment
-import com.example.myapplication.view.navigation.MarsFragment
-import com.example.myapplication.view.navigation.SystemFragment
 
 class ViewPagerAdapter(fragmentManager: FragmentManager): FragmentStatePagerAdapter(fragmentManager) {
 
