82d13bdd533c69ef009ebef0909a37fae20f6dcc
whittema <shamarsen@mail.ru>
2023-02-12 16:55:21 +0300

    урок 4| motion layout (2)

--- app/src/main/java/com/example/myapplication/view/layouts/MotionFragment.kt
+++ app/src/main/java/com/example/myapplication/view/layouts/MotionFragment.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.myapplication.databinding.FragmentMotionStartBinding
+import com.example.myapplication.databinding.FragmentMotionBinding
 
 class MotionFragment : Fragment() {
-    private var _binding: FragmentMotionStartBinding? = null
-    private val binding: FragmentMotionStartBinding
+    private var _binding: FragmentMotionBinding? = null
+    private val binding: FragmentMotionBinding
         get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMotionStartBinding.inflate(inflater, container, false)
+        _binding = FragmentMotionBinding.inflate(inflater, container, false)
         return binding.root
     }
     override fun onDestroy() {
