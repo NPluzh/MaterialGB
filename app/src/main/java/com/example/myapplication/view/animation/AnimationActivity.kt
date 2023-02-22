9038b52f504bd75bcdedfcb54997baeeed070c55
whittema <shamarsen@mail.ru>
2023-02-22 09:28:49 +0300

    урок 5| add transitions

---
+++ app/src/main/java/com/example/myapplication/view/animation/AnimationActivity.kt
@@ -0,0 +1,42 @@
+package com.example.myapplication.view.animation
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.transition.ChangeBounds
+import androidx.transition.Fade
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
+import com.example.myapplication.databinding.ActivityAnimationBinding
+
+class AnimationActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityAnimationBinding
+
+    var isFlag = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        super.onCreate(savedInstanceState)
+        binding = ActivityAnimationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.button.setOnClickListener {
+            isFlag = !isFlag
+            val myAutoTransition = TransitionSet()
+            myAutoTransition.ordering = TransitionSet.ORDERING_TOGETHER
+            val fade = Fade()
+            val changeBounds = ChangeBounds()
+            myAutoTransition.addTransition(fade)
+            myAutoTransition.addTransition(changeBounds)
+            TransitionManager.beginDelayedTransition(binding.transitionsContainer,myAutoTransition)
+            binding.text.visibility = if(isFlag) View.VISIBLE else {
+                View.GONE}
+        }
+
+    }
+}
