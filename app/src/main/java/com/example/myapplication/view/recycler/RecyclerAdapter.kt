dd50b68812fb72f3b39304adc590d9b1ae7972f2
whittema <shamarsen@mail.ru>
2023-02-27 09:47:52 +0300

    урок 6| difutils

--- app/src/main/java/com/example/myapplication/view/recycler/RecyclerAdapter.kt
+++ app/src/main/java/com/example/myapplication/view/recycler/RecyclerAdapter.kt
@@ -3,23 +3,38 @@ package com.example.myapplication.view.recycler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityRecyclerItemEarthBinding
 import com.example.myapplication.databinding.ActivityRecyclerItemHeaderBinding
 import com.example.myapplication.databinding.ActivityRecyclerItemMarsBinding
+import com.example.myapplication.view.recycler.difutil.Change
+import com.example.myapplication.view.recycler.difutil.DiffUtilCallback
+import com.example.myapplication.view.recycler.difutil.createCombinedPayload
 
-class RecyclerAdapter(private var listData: MutableList<Pair<Data,Boolean>>,val callbackAdd: AddItem,val callbackRemove: RemoveItem) :
-    RecyclerView.Adapter<RecyclerAdapter.BaseViewHolder>(),ItemTouchHelperAdapter {
+class RecyclerAdapter(
+    private var listData: MutableList<Pair<Data, Boolean>>,
+    val callbackAdd: AddItem,
+    val callbackRemove: RemoveItem
+) :
+    RecyclerView.Adapter<RecyclerAdapter.BaseViewHolder>(), ItemTouchHelperAdapter {
 
 
-    fun setListDataRemove(listDataNew: MutableList<Pair<Data,Boolean>>,position: Int){
+    fun setListDataForDiffUtil(listDataNew: MutableList<Pair<Data, Boolean>>) {
+        val diff = DiffUtil.calculateDiff(DiffUtilCallback(listData, listDataNew))
+        diff.dispatchUpdatesTo(this)
+        listData= listDataNew
+    }
+
+    fun setListDataRemove(listDataNew: MutableList<Pair<Data, Boolean>>, position: Int) {
         listData = listDataNew
         notifyItemRemoved(position)
     }
 
-    fun setListDataAdd(listDataNew: MutableList<Pair<Data,Boolean>>,position: Int){
+    fun setListDataAdd(listDataNew: MutableList<Pair<Data, Boolean>>, position: Int) {
         listData = listDataNew
         notifyItemInserted(position)
     }
@@ -52,17 +67,28 @@ class RecyclerAdapter(private var listData: MutableList<Pair<Data,Boolean>>,val 
         holder.bind(listData[position])
     }
 
+    override fun onBindViewHolder(
+        holder: BaseViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        if (payloads.isEmpty()) {
+            super.onBindViewHolder(holder, position, payloads)
+        } else {
+            val createCombinedPayload = createCombinedPayload(payloads as List<Change<Pair<Data, Boolean>>>)
+            if(createCombinedPayload.newData.first.name!=createCombinedPayload.oldData.first.name) // в данном случае это лишнее
+                holder.itemView.findViewById<TextView>(R.id.name).text =createCombinedPayload.newData.first.name
+        }
+    }
+
     override fun getItemCount(): Int {
         return listData.size
     }
 
 
-
-
-
     inner class MarsViewHolder(val binding: ActivityRecyclerItemMarsBinding) :
         BaseViewHolder(binding.root) {
-        override fun bind(data: Pair<Data,Boolean>) {
+        override fun bind(data: Pair<Data, Boolean>) {
             binding.name.text = data.first.name
             binding.addItemImageView.setOnClickListener {
                 callbackAdd.add(layoutPosition)
@@ -74,19 +100,20 @@ class RecyclerAdapter(private var listData: MutableList<Pair<Data,Boolean>>,val 
             binding.moveItemUp.setOnClickListener {
                 // TODO HW java.lang.IndexOutOfBoundsException: Index: -1, Size: 7
                 listData.removeAt(layoutPosition).apply {
-                    listData.add(layoutPosition-1,this)
+                    listData.add(layoutPosition - 1, this)
                 }
-                notifyItemMoved(layoutPosition,layoutPosition-1)
+                notifyItemMoved(layoutPosition, layoutPosition - 1)
             }
 
             binding.moveItemDown.setOnClickListener {
                 // TODO HW java.lang.IndexOutOfBoundsException: Index: 8, Size: 7
                 listData.removeAt(layoutPosition).apply {
-                    listData.add(layoutPosition+1,this)
+                    listData.add(layoutPosition + 1, this)
                 }
-                notifyItemMoved(layoutPosition,layoutPosition+1)
+                notifyItemMoved(layoutPosition, layoutPosition + 1)
             }
-            binding.marsDescriptionTextView.visibility = if(listData[layoutPosition].second) View.VISIBLE else View.GONE
+            binding.marsDescriptionTextView.visibility =
+                if (listData[layoutPosition].second) View.VISIBLE else View.GONE
 
             binding.marsImageView.setOnClickListener {
                 listData[layoutPosition] = listData[layoutPosition].let {
@@ -101,26 +128,23 @@ class RecyclerAdapter(private var listData: MutableList<Pair<Data,Boolean>>,val 
     }
 
 
-
-
-
     class EarthViewHolder(val binding: ActivityRecyclerItemEarthBinding) :
         BaseViewHolder(binding.root) {
-        override fun bind(data: Pair<Data,Boolean>) {
+        override fun bind(data: Pair<Data, Boolean>) {
             binding.name.text = data.first.name
         }
     }
 
     class HeaderViewHolder(val binding: ActivityRecyclerItemHeaderBinding) :
         BaseViewHolder(binding.root) {
-        override fun bind(data: Pair<Data,Boolean>) {
+        override fun bind(data: Pair<Data, Boolean>) {
             binding.name.text = data.first.name
         }
     }
 
     abstract class BaseViewHolder(view: View) :
-        RecyclerView.ViewHolder(view),ItemTouchHelperViewHolder {
-        abstract fun bind(data: Pair<Data,Boolean>)
+        RecyclerView.ViewHolder(view), ItemTouchHelperViewHolder {
+        abstract fun bind(data: Pair<Data, Boolean>)
         override fun onItemSelect() {
             itemView.setBackgroundColor(ContextCompat.getColor(itemView.context, R.color.my_color))
         }
@@ -132,9 +156,9 @@ class RecyclerAdapter(private var listData: MutableList<Pair<Data,Boolean>>,val 
 
     override fun onItemMove(fromPosition: Int, toPosition: Int) {
         listData.removeAt(fromPosition).apply {
-            listData.add(toPosition,this)
+            listData.add(toPosition, this)
         }
-        notifyItemMoved(fromPosition,toPosition)
+        notifyItemMoved(fromPosition, toPosition)
     }
 
     override fun onItemDismiss(position: Int) {
